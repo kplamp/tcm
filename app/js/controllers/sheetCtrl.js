6d5ecdaa273c8f482fff42e21c1a28fdb82d631c
Steve Schroeder <tekknoschtev@gmail.com>
2013-10-23 20:32:36 -0400

    Updated to support testPlan parameters.

--- app/js/controllers/sheetCtrl.js
+++ app/js/controllers/sheetCtrl.js
@@ -1,24 +1,19 @@
 var tcm = angular.module('tcm');
 
 tcm.controller('testPlanCtrl', function($scope) {
-  
-	var steps = [
-		{
-			setup: "1. View only and modifiable security is disabled for maintaining the purge prevention checkbox.",
-			action: "1. Close the problem report\n2. Update some junk",
-			outcome: "test",
-			result: ''
-		}
-  ];
 	
   $scope.mode = 'edit';
   $scope.format = '';
   $scope.results = '';
   $scope.cols = ['Setup','Action','Expected Outcome','Result'];
-  $scope.testPlan = [];
+  $scope.testPlan = {};
+  $scope.testPlan.extrnId = "SFS-12345";
+  $scope.testPlan.softwareChange = "";
+  $scope.testPlan.testStrategy = "";
+  $scope.testPlan.comments = "";
   $scope.testPlan.category = [];
+//  $scope.testPlan.category.push({name: "test", testSteps: []});
 	
-	$scope.testPlan.push({name: "Category 1", testSteps: steps});
 	
   $scope.setMode = function(mode) {
     $scope.mode = mode;
@@ -28,29 +23,30 @@ tcm.controller('testPlanCtrl', function($scope) {
   };
   
   $scope.setResult = function(category, step, result) {
-		$scope.testPlan[category].testSteps[step].result = result;
+		$scope.testPlan.category[category].testSteps[step].result = result;
   };
   
   $scope.addStep = function(index) {
-		$scope.testPlan[index].testSteps.push({setup: '', action: '', outcome: ''});
+		$scope.testPlan.category[index].testSteps.push({setup: '', action: '', outcome: ''});
   };
 	
 	$scope.addCategory = function() {
-		$scope.testPlan.push({name: '', testSteps: [{setup: '', action: '', outcome: ''}]});
+    console.log($scope.testPlan);
+		$scope.testPlan.category.push({name: '', testSteps: [{setup: '', action: '', outcome: ''}]});
 	};
   
   $scope.removeStep = function(category, step) {
-		$scope.testPlan[category].testSteps.splice(step, 1);
+		$scope.testPlan.category[category].testSteps.splice(step, 1);
   };
 	
 	$scope.removeCategory = function(category) {
-		$scope.testPlan.splice(category, 1);
+		$scope.testPlan.category.splice(category, 1);
 	}
   
   $scope.setResultAll = function(result) {
-    for(i=0; i<$scope.testPlan.length; i++) {
-      for(j=0; j<$scope.testPlan[i].testSteps.length; j++) {
-        $scope.testPlan[i].testSteps[j].result = result;
+    for(i=0; i<$scope.testPlan.category.length; i++) {
+      for(j=0; j<$scope.testPlan.category[i].testSteps.length; j++) {
+        $scope.testPlan.category[i].testSteps[j].result = result;
       }
     };
   };
